adde0f7214e17929cbd7ae326344fca143e8dfd9
lui5c <luiscollado26@gmail.com>
2022-03-12 16:04:33 -0500

    progress

--- journalentries/views.py
+++ journalentries/views.py
@@ -8,14 +8,12 @@ def index(request):
     return HttpResponse("Hello, world. You're at the journalentries index.")
 
 def single_entry_view(request, date):
-    try:
-        found = Entry.objects.filter(entry_date=date).first()
-        context = {
-            'date': found.entry_date,
-            'text': found.entry_text,
-            'title': found.entry_title,
-            }
-        return render(request, 'journalentries/index.html', context)
-    except:
-        return HttpResponse("You fucked up")
-    
+    found = Entry.objects.filter(entry_date=date).first()
+    #plist is a list of paragraphs
+    plist = found.entry_text.split("\n")
+    context = {
+        'date': found.entry_date,
+        'text': plist,
+        'title': found.entry_title,
+        }
+    return render(request, 'journalentries/singleentry.html', context)
